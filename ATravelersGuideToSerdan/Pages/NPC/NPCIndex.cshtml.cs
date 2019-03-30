7add96147fb8af13ef4507dba74a30eb5835f7c0
Christian Levin <christian.johansson@gmail.com>
2019-03-31 00:49:40 +0100

    creating a page displaying multiple Npc:s

--- ATravelersGuideToSerdan/Pages/NPC/NPCIndex.cshtml.cs
+++ ATravelersGuideToSerdan/Pages/NPC/NPCIndex.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ATravelersGuideToSerdan.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,38 @@ namespace ATravelersGuideToSerdan.Pages.NPC
 {
     public class NPCIndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private ISerdan _Db;
 
+        public NPCIndexModel(ISerdan Db)
+        {
+            _Db = Db;
         }
+
+        public IActionResult OnGet(string race)
+        {
+            if (race == null)
+            {
+                return Page();
+            }
+            else
+            {
+                List<Models.NPC> NpcOfARace = new List<Models.NPC>();
+
+                NpcOfARace = _Db.GetMultipleNPCs(race);
+                List<ViewModels.NpcListViewModel> compactNpcList = new List<ViewModels.NpcListViewModel>();
+                foreach (var npc in NpcOfARace)
+                {
+
+                }
+                return RedirectToPage("GetNpcList", NpcOfARace);
+            }
+        }
+
+
+        //public List<Models.NPC> PageResult GetCharacters(string race)
+        //{
+        //    List<Models.NPC> NpcOfARace = _Db.GetMultipleNPCs(race);
+        //    return Page()
+        //}
     }
 }
