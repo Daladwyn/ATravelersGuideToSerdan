dc988641a4d0f805a1e004e0d3f925f21f41b8f8
Christian Levin <christian.johansson@gmail.com>
2019-05-02 00:35:55 +0200

    Started to build the page to note meetings

---
+++ ATravelersGuideToSerdan/Pages/Rendezvous/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ATravelersGuideToSerdan.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ATravelersGuideToSerdan.Pages.Rendezvous
+{
+    public class IndexModel : PageModel
+    {
+        public List<NpcListViewModel> Npc1 = new List<NpcListViewModel>();
+        public List<NpcListViewModel> Npc2 = new List<NpcListViewModel>();
+        public string MeetingDetails = "";
+        public List<PlanetViewModel> Planet = new List<PlanetViewModel>();
+        public string Place = "";
+
+
+        
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
