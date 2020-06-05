9385285733732e26e60a9e7115e838804ebdc836
Christian Levin <christian.johansson@gmail.com>
2020-06-05 22:21:31 +0200

    Correcting Talent class

--- ATravelersGuideToSerdan/Models/TalentSkill.cs
+++ ATravelersGuideToSerdan/Models/TalentSkill.cs
@@ -8,14 +8,15 @@ namespace ATravelersGuideToSerdan.Models
     public class Talent
     {
         public int TalentId { get; set; }
-        public int TalentIsBasedOn { get; set; }
+        public string TalentIsBasedOn { get; set; }
         public string TalentName { get; set; }
         public int TalentCost1 { get; set; }
         public bool TalentCostFysical { get; set; }
+        public int NumberOfRequirements { get; set; }
         public int TalenReqAttribute1 { get; set; }
-        public string TalentReqAtributeValue1 { get; set; }
+        public string TalentReqAttributeValue1 { get; set; }
         public int TalenReqAttribute2 { get; set; }
-        public string TalentReqAtributeValue2 { get; set; }
+        public string TalentReqAttributeValue2 { get; set; }
         public string TalentReqTalentSkill1 { get; set; }
         public string TalentReqTalentSkill2 { get; set; }
         public string TalentDescription { get; set; }
